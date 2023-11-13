996a88d91383ba1e6062081af3f160ef2bba50dc
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-11-13 17:12:13 +0100

    Change CCA3 code for country name

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region, SmallCountry } from '../../interfaces/country.interfaces';
-import { switchMap, tap } from 'rxjs/operators';
+import { filter, switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'countries-selector-page',
@@ -11,11 +11,12 @@ import { switchMap, tap } from 'rxjs/operators';
 })
 export class SelectorPageComponent implements OnInit {
   public countriesByRegion: SmallCountry[] = [];
+  public borders: SmallCountry[] = [];
 
   public myForm: FormGroup = this.fb.group({
     region: ['', [Validators.required]],
     country: ['', [Validators.required]],
-    borders: ['', [Validators.required]],
+    border: ['', [Validators.required]],
   });
 
   constructor(
@@ -25,6 +26,7 @@ export class SelectorPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.onRegionChange();
+    this.onCountryChange();
   }
 
   get regions(): Region[] {
@@ -36,10 +38,27 @@ export class SelectorPageComponent implements OnInit {
       .get('region')!
       .valueChanges.pipe(
         tap(() => this.myForm.get('country')!.setValue('')),
+        tap(() => (this.borders = [])),
         switchMap((region) =>
           this.countriesService.getCountriesByRegion(region)
         )
       )
       .subscribe((countries) => (this.countriesByRegion = countries));
+  }
+
+  onCountryChange(): void {
+    this.myForm
+      .get('country')!
+      .valueChanges.pipe(
+        tap(() => this.myForm.get('border')!.setValue('')),
+        filter((value: string) => value.length > 0),
+        switchMap((alphaCode) =>
+          this.countriesService.getCountryByAlphaCode(alphaCode)
+        ),
+        switchMap((country) =>
+          this.countriesService.getCountryBordersByCode(country.borders)
+        )
+      )
+      .subscribe((countries) => (this.borders = countries));
   }
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -4,7 +4,7 @@ import {
   Region,
   SmallCountry,
 } from '../interfaces/country.interfaces';
-import { Observable, map, of, tap } from 'rxjs';
+import { Observable, combineLatest, map, of, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -41,5 +41,29 @@ export class CountriesService {
         }))
       )
     );
+  }
+
+  getCountryByAlphaCode(alphaCode: string): Observable<SmallCountry> {
+    const url = `${this.baseUrl}/alpha/${alphaCode}?fields=cca3,name,borders`;
+    return this.http.get<Country>(url).pipe(
+      map((country) => ({
+        name: country.name.common,
+        cca3: country.cca3,
+        borders: country.borders ?? [],
+      }))
+    );
+  }
+
+  getCountryBordersByCode(borders: string[]): Observable<SmallCountry[]> {
+    if (!borders || borders.length === 0) return of([]);
+
+    const countriesRequests: Observable<SmallCountry>[] = [];
+
+    borders.forEach((code) => {
+      const request = this.getCountryByAlphaCode(code);
+      countriesRequests.push(request);
+    });
+
+    return combineLatest(countriesRequests);
   }
 }
